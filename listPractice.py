922981e0c3da9cd498e0cd07cbf4c6d47abe5990
Parker Bruce <brucepa@vcu.edu>
2020-12-21 12:59:49 -0500

    list practice in python

--- listPractice.py
+++ listPractice.py
@@ -50,3 +50,37 @@ def combine_sort(lst1, lst2):
   return combined_lst
 
 #print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+#Create a function named remove_middle which has three parameters named lst, start, and end.
+#The function should return a list where all elements in lst with an index between start and end (inclusive) have been removed.
+#For example, the following code should return [4, 23, 42] because elements at indices 1, 2, and 3 have been removed:
+
+def remove_middle(lst, start, end):
+  return lst[:start] + lst[end+1:]
+
+#Uncomment the line below when your function is done
+#print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
+
+#The function should return a new list where all elements are the same as in lst except for the element 
+# at index. The element at index should be double the value of the element at index of the original lst.
+
+def double_index(lst, index):
+  if index < len(lst):
+    lst[index] = lst[index] * 2
+    return lst
+  else:
+    return lst
+
+#print(double_index([3, 8, -10, 12], 2))
+
+#Create a function called middle_element that has one parameter named lst.
+#If there are an odd number of elements in lst, the function should return the middle element. If there
+#are an even number of elements, the function should return the average of the middle two elements
+def middle_element(lst):
+  if len(lst) % 2 == 0:
+    sum = lst[int(len(lst)/2)] + lst[int(len(lst)/2) - 1]
+    return sum / 2
+  else:
+    return lst[int(len(lst)/2)]
+
+#print(middle_element([5, 2, -10, -4, 4, 5]))
